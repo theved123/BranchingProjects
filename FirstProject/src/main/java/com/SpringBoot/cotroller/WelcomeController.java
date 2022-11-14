c643dc2dd585048fa6dd9f687429d75205d69d4a
theved123 <vedpgaikwad@gmail.com>
2022-11-14 15:39:00 +0530

    Welcome Controller Added

---
+++ FirstProject/src/main/java/com/SpringBoot/cotroller/WelcomeController.java
@@ -0,0 +1,10 @@
+package com.SpringBoot.cotroller;
+
+public class WelcomeController {
+
+	public static void main(String[] args) {
+		String s = "Welcome Controller";
+		System.out.println(s);
+	}
+
+}
